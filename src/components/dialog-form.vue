7ff0ed20add3a26450efd81f5ab9bece22404736
gaabrielbrocco <gabsbrocco@gmail.com>
2024-05-09 17:31:36 -0300

    ✨feat: Ajusta função de calculo de juros e inicia data table

--- src/components/dialog-form.vue
+++ src/components/dialog-form.vue
@@ -9,60 +9,96 @@
           <v-row class="mt-5">
             <v-col cols="6">
               <div>Valor do aporte inicial:</div>
-              <v-text-field variant="outlined" type="number" v-model="aporteInicial">
+              <v-text-field
+                variant="outlined"
+                type="number"
+                v-model.number="aporteInicial"
+              >
               </v-text-field>
             </v-col>
             <v-col cols="6">
               <div>Aporte mensal:</div>
-              <v-text-field variant="outlined" type="number" v-model="aporteMensal">
+              <v-text-field
+                variant="outlined"
+                type="number"
+                v-model.number="aporteMensal"
+              >
               </v-text-field>
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="6">
               <div>Taxa de juros ao ano:</div>
-              <v-text-field variant="outlined" type="number" label="%" v-model="taxaJuros">
-
+              <v-text-field
+                variant="outlined"
+                type="number"
+                label="%"
+                v-model.number="taxaJuros"
+              >
               </v-text-field>
             </v-col>
             <v-col cols="6">
-              <div>Inflação ao ano:</div>
-              <v-text-field variant="outlined" type="number" label="%" v-model="inflacao">
-
+              <div>Determine o período em meses:</div>
+              <v-text-field
+                variant="outlined"
+                type="number"
+                v-model.number="periodo"
+              >
               </v-text-field>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col cols="6">
-              <div>Determine o período em anos:</div>
-              <v-text-field variant="outlined" type="number" v-model="periodo">
-              </v-text-field>
-            </v-col>
-          </v-row>
-
           <div class="d-flex justify-center mt-5">
-            <v-btn size="large" color="green" text="calcular" @click="calculaJuros"></v-btn>
+            <v-btn
+              size="large"
+              color="green"
+              text="calcular"
+              @click="calculaJuros"
+            ></v-btn>
           </div>
         </v-form>
       </v-col>
+    </v-row>
+    <v-row>
+      <v-data-table
+          density="compact"
+          fixed-header
+          hover
+          items-per-page="25"
+          :headers="colunasTabela.value"
+          :items="resultados.value"
+        >
+      </v-data-table>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
+import { colunas } from "../const/colunas";
 
-const aporteInicial = ref(0)
-const aporteMensal = ref(0)
-const taxaJuros = ref(0)
-const inflacao = ref(0)
-const periodo = ref(0)
+const aporteInicial = ref(0);
+const aporteMensal = ref(0);
+const taxaJuros = ref(0);
+const periodo = ref(0);
+const resultados = ref([]);
+const colunasTabela = ref(colunas);
 
 
 const calculaJuros = () => {
-  const valorFinal = aporteMensal.value * [(1 + taxaJuros.value) ^ (periodo.value - 1)] / taxaJuros.value
-  return valorFinal
-}
 
+  let saldo = aporteInicial.value;
+  let porcentagemTaxa = taxaJuros.value / 100 / 12;
+  let resultado = [];
+
+  for (let i = 0; i < periodo.value; i++) {
+    saldo = saldo * (1 + porcentagemTaxa) + aporteMensal.value;
+    resultado.push((saldo - aporteMensal.value).toFixed(2));
+  }
+
+  resultados.value =  resultado;
+  console.log( resultados.value);
+
+};
+  // F = P*(1+J)N
 
 </script>
---
+++ src/const/colunas.js
@@ -0,0 +1,24 @@
+export const colunas = [
+    // {
+    //   title: "Meses",
+    //   key: "meses",
+    // },
+    // {
+    //   title: "Valor inicial",
+    //   key: "valorInicial",
+    // },
+    // {
+    //   title: "Aporte",
+    //   key: "aporte",
+    // },
+    // {
+    //   title: "Rendimento",
+    //   key: "rendimento",
+    // },
+    {
+      title: "Valor final",
+      key: "valorFinal",
+    },
+  
+  ];
+  
